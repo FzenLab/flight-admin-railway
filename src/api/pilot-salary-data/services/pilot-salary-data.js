2e663c6273ff7801b7d13c859e5964af77b6e048
Federico <fede.senmartin@gmail.com>
2023-10-31 16:17:52 -0300

    test schema

---
+++ src/api/pilot-salary-data/services/pilot-salary-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pilot-salary-data service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::pilot-salary-data.pilot-salary-data');
