0dbf3f63f5cf62d0122d3f5ccb093ccf84c3dfd9
Federico <fede.senmartin@gmail.com>
2023-10-31 16:24:49 -0300

    test

--- src/index.js
+++ src/index.js
@@ -8,99 +8,99 @@ module.exports = {
 
 
     // Custom query resolver to get all authors and their details.
-    extensionService.use(({ strapi }) => ({
-      typeDefs: `
-
-      type PilotData {
-        pilot: PilotEntity
-        balance: Balance
-
-      }
-
-      type Balance {
-        total_balance: Float
-        detail: [FlightDetail]
-
-      }
-
-      type FlightDetail { 
-        balance: Float
-        planeCode: String
-      }
-
-        type Query {
-          listPilotsBalance: [PilotData]
-        }
-      `,
-
-      resolvers: {
-        Query: {
-          listPilotsBalance: {
-            resolve: async (parent, args, context) => {
-              var pilotData = [];
-
-              const pilots = await strapi.entityService.findMany('api::pilot.pilot', {
-                populate: "*",
-
-              });
-
-              const entries = await strapi.entityService.findMany('api::flight-record.flight-record', {
-                populate: "*",
-
-              });
-                
-              pilots.forEach(pilot => {
-                var details = [];
-                var planesID = [];
-                var planeData = { 
-                  balance : 0
-                }
-
-                var pilotBalance = 0;
-                entries.forEach(entrie => {
-                  if(entrie.pilot.id == pilot.id){
-                    pilotBalance = pilotBalance + entrie.hours
-                    if(!planesID.includes(entrie.plane.code)){
-                      planesID.push(entrie.plane.code)
-                    }
-
-                  } 
-                });
-
-                planesID.forEach(planeId => {
-                  var planeBalance = 0
-                  entries.forEach(entrie => {
-                    if(planeId == entrie.plane.code && entrie.pilot.id == pilot.id ){
-                      planeBalance =  planeBalance + entrie.hours
-                    }
-                  });
-                  details.push({
-                    planeCode : planeId,
-                    balance : planeBalance
-                  })
-                });
-               
-                pilotData.push({
-                  pilot :pilot,
-                  balance : {
-                    total_balance :pilotBalance * -1,
-                    detail :details
-                    
-                  }
-                })
-              });
-
-              return pilotData;
-            },
-          },
-        }
-      },
-
-      resolversConfig: {
-        "Query.listPilotsBalance": {
-          auth: false,
-        },
-      },
-    }));
+//    extensionService.use(({ strapi }) => ({
+//      typeDefs: `
+//
+//      type PilotData {
+//        pilot: PilotEntity
+//        balance: Balance
+//
+//      }
+//
+//      type Balance {
+//        total_balance: Float
+//        detail: [FlightDetail]
+//
+//      }
+//
+//      type FlightDetail {
+//        balance: Float
+//        planeCode: String
+//      }
+//
+//        type Query {
+//          listPilotsBalance: [PilotData]
+//        }
+//      `,
+//
+//      resolvers: {
+//        Query: {
+//          listPilotsBalance: {
+//            resolve: async (parent, args, context) => {
+//              var pilotData = [];
+//
+//              const pilots = await strapi.entityService.findMany('api::pilot.pilot', {
+//                populate: "*",
+//
+//              });
+//
+//              const entries = await strapi.entityService.findMany('api::flight-record.flight-record', {
+//                populate: "*",
+//
+//              });
+//
+//              pilots.forEach(pilot => {
+//                var details = [];
+//                var planesID = [];
+//                var planeData = {
+//                  balance : 0
+//                }
+//
+//                var pilotBalance = 0;
+//                entries.forEach(entrie => {
+//                  if(entrie.pilot.id == pilot.id){
+//                    pilotBalance = pilotBalance + entrie.hours
+//                    if(!planesID.includes(entrie.plane.code)){
+//                      planesID.push(entrie.plane.code)
+//                    }
+//
+//                  }
+//                });
+//
+//                planesID.forEach(planeId => {
+//                  var planeBalance = 0
+//                  entries.forEach(entrie => {
+//                    if(planeId == entrie.plane.code && entrie.pilot.id == pilot.id ){
+//                      planeBalance =  planeBalance + entrie.hours
+//                    }
+//                  });
+//                  details.push({
+//                    planeCode : planeId,
+//                    balance : planeBalance
+//                  })
+//                });
+//
+//                pilotData.push({
+//                  pilot :pilot,
+//                  balance : {
+//                    total_balance :pilotBalance * -1,
+//                    detail :details
+//
+//                  }
+//                })
+//              });
+//
+//              return pilotData;
+//            },
+//          },
+//        }
+//      },
+//
+//      resolversConfig: {
+//        "Query.listPilotsBalance": {
+//          auth: false,
+//        },
+//      },
+//    }));
   },
 };
