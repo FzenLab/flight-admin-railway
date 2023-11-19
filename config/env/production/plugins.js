42c48f687bcf306ef0c90cc00ceeb732cf071cf1
Federico <fede.senmartin@gmail.com>
2023-11-18 23:12:44 -0300

    no message

---
+++ config/env/production/plugins.js
@@ -0,0 +1,36 @@
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+      jwtSecret: env('JWT_SECRET'),
+      },
+    },
+    graphql: {
+        enabled : true,
+        config: {
+          endpoint: '/graphql',
+          shadowCRUD: true,
+          playgroundAlways: true,
+          depthLimit: 7,
+          amountLimit: 101,
+          apolloServer: {
+            tracing: false,
+            introspection: true,
+          },
+        },
+      },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+
