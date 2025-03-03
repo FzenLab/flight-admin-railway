147a97394ccc4513b9b1c678336c2a6054680621
Federico <fede.senmartin@gmail.com>
2025-03-03 17:20:37 -0300

    no message

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '0.0.0.0'),
+      host: env('PGHOST', 'coolify.fzen-labs.site'),
       port: env.int('PGPORT', 5432),
       database: env('PGDATABASE', 'postgres'),
       user: env('PGUSER', 'postgres'),
