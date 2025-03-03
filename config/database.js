656429763711981da3be1196f5357bf2ea827c02
Federico <fede.senmartin@gmail.com>
2025-03-03 17:12:35 -0300

    no message

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
+      host: env('PGHOST', '0.0.0.0'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('PGDATABASE', 'postgres'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', '5fYmvLvqcIGYBkWqcTSO32wD1CsXe17dwKcJCE5sGoew5MTLearRBJkvhTECaVYK'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
